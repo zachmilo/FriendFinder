3cffdac9b694ed93b27f5f8427f4a4f524a738f1
zachmilo <zachmilo45@gmail.com>
2017-05-14 21:13:58 -0400

    setting up backend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,36 @@
 "use strict";
 
-var path = require("path");
-var	fs = require("fs");
-
 module.exports = function(app) {
-	app.get("/", function(req, res) {
-		res.sendFile(path.join(__dirname, "../public/home.html"));
-	});
 
-	app.get("/survey", function (req, res) {
-		res.sendFile(path.join(__dirname, "../public/survey.html"));
-	});
+    app.get("/api/tables", function(req, res) {
+        var tables = [];
+        for (var i = 0; i < 5; i++) {
+            tables.push(tablesArray[i]);
+        }
+        // console.log(tables);
+        res.json(tables);
+    });
+
+    app.get("/api/waitlist", function(req, res) {
+        var waitList = [];
+        for (var i = 5; i < tablesArray.length; i++) {
+           if (!null) waitList.push(tablesArray[i]);
+        }
+        // console.log(waitList);
+        res.json(waitList);
+    });
+
+    app.post("/api/tables", function(req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        var newTable = req.body;
+
+        // console.log(newTable);
+        // console.log("tables");
+
+        // We then add the json the user sent to the character array
+        tablesArray.push(newTable);
+
+        // We then display the JSON to the users
+        res.json(newTable);
+    });
 }
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var path = require("path");
+var	fs = require("fs");
+
+module.exports = function(app) {
+	app.get("/", function(req, res) {
+		res.sendFile(path.join(__dirname, "../public/home.html"));
+	});
+
+	app.get("/survey", function (req, res) {
+		res.sendFile(path.join(__dirname, "../public/survey.html"));
+	});
+}
